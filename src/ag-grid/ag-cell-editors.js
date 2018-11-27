cc879a6fdc372b899fe447bb07ff0bf817d12c0b
Varun Dev <varun.dev@hotmail.com>
2018-11-27 16:01:44 +0000

    added a editor wrapper

--- src/ag-grid/ag-cell-editors.js
+++ src/ag-grid/ag-cell-editors.js
@@ -1,19 +1,17 @@
 import { TextCellEditor, PopupTextCellEditor } from 'ag-grid-community'
 
-export function TextEditor(onChange) {
-  return class TextEditor extends TextCellEditor {
+function wrapEditor(parent, onChange) {
+  return class EditorWrapper extends parent {
     isCancelAfterEnd () {
-      onChange(this.id, this.colId, this.getValue())
+      onChange(
+        this.params.node.id,
+        this.params.column.getId(),
+        this.getValue()
+      )
       return true;
     }
   }
 }
 
-export function PopupTextEditor(onChange) {
-  return class PopupTextEditor extends PopupTextCellEditor {
-    isCancelAfterEnd () {
-      onChange(this.id, this.colId, this.getValue())
-      return true;
-    }
-  }
-}
+export const TextEditor = onChange => wrapEditor(TextCellEditor, onChange)
+export const PopupTextEditor = onChange => wrapEditor(PopupTextCellEditor, onChange)
